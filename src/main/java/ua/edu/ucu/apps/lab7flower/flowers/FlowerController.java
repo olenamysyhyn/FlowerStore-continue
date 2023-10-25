d54a17a356e639d4ef098988d492dee87001c8d3
olenamysyshyn <mysyshyn.pn@ucu.edu.ua>
2023-10-26 00:34:56 +0300

    changes

--- src/main/java/ua/edu/ucu/apps/lab7flower/flowers/FlowerController.java
+++ src/main/java/ua/edu/ucu/apps/lab7flower/flowers/FlowerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
+
 @RestController
 @RequestMapping("/api/flowers")
 public class FlowerController {
--- src/main/java/ua/edu/ucu/apps/lab7flower/flowers/Lab7flowerApplication.java
+++ src/main/java/ua/edu/ucu/apps/lab7flower/flowers/Lab7flowerApplication.java
@@ -2,7 +2,7 @@ package ua.edu.ucu.apps.lab7flower.flowers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
